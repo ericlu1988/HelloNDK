40a873feb4384594445f25806dcfc54bf2f190ff
lujun <952344450@qq.com>
2017-04-09 13:43:34 +0800

    增加函数

--- app/src/main/java/com/example/eric/newlearnandroid/MainActivity.java
+++ app/src/main/java/com/example/eric/newlearnandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.eric.newlearnandroid;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myMethord(R.layout.class.toString());
+    }
+    //新建工程方法
+    protected  void myMethord(String str){
+        System.out.println(str);
     }
 }
