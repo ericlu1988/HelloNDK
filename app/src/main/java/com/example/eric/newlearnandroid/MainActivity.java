8a8e7f4290f381cf69d5e07fec5b47dd1cf36d3b
lujun <952344450@qq.com>
2017-04-09 13:39:11 +0800

    增加！！！

--- app/src/main/java/com/example/eric/newlearnandroid/MainActivity.java
+++ app/src/main/java/com/example/eric/newlearnandroid/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//新加入注释
+//新加入注释!!!!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
